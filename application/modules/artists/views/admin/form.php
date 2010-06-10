74e29c4090ff87d7f6b397aa8145a7f71a88b91c
Ding-Wen Chen <dingwen70@gmail.com>
2010-06-10 12:09:05 -0700

    Add Artists Module - Now is only for single artists.

---
+++ application/modules/artists/views/admin/form.php
@@ -0,0 +1,12 @@
+<div id="form">
+    <form action="<?php echo site_url(uri_string()); ?>" method="post">
+        <div>
+            <label>First Name</label>
+            <?php echo form_input(); ?>
+        </div>
+        <div>
+            <label>First Name</label>
+            <?php echo form_input(); ?>
+        </div>
+    </form>
+</div>
